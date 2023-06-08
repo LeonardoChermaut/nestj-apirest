f697010e2ca66d15a33e34857cafd72b2f02e47d
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-08 11:40:00 -0300

    feat: added global filter exception

---
+++ src/infra/exception/http.execption.filter.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './http.execption.filter';
+
+@Module({
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
+})
+export class HttpExceptionModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './domain/view/app/app.module';
+import { HttpExceptionFilter } from './infra/exception/http.execption.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();

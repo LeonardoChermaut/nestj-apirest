3410ea20639e2076d85234c1868f52108f7e2a59
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-08 11:49:03 -0300

    refactor: import orm config from module

--- src/domain/view/app/app.module.ts
+++ src/domain/view/app/app.module.ts
@@ -1,18 +1,17 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from 'src/domain/models/users/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PostsModule } from 'src/domain/models/posts/post.module';
-import TypeOrmConfiguration from 'src/infra/config/typeorm.config';
+import { TypeOrmConfigurationModule } from 'src/infra/config/typeor.config.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UsersModule,
     PostsModule,
-    TypeOrmModule.forRoot(TypeOrmConfiguration),
+    TypeOrmConfigurationModule,
   ],
   controllers: [AppController],
   providers: [AppService],

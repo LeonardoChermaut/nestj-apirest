fb55498a833a4fd0dec01ace68c8ebd815bc059c
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-09 23:16:29 -0300

    chore: added lib class validator

--- src/domain/models/users/user.entity.ts
+++ src/domain/models/users/user.entity.ts
@@ -1,21 +1,28 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Post } from '../posts/post.entity';
+import { IsNotEmpty, IsEmail, MinLength, MaxLength } from 'class-validator';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn({ type: 'integer' })
   id: number;
 
-  @Column({ length: 80, nullable: false })
+  @Column({ length: 80 })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
   name: string;
 
-  @Column({ length: 100, unique: true, nullable: false })
+  @Column({ length: 100, unique: true })
+  @IsNotEmpty({ message: 'O e-mail é obrigatório' })
+  @IsEmail({}, { message: 'Informe um e-mail válido' })
   email: string;
 
-  @Column({ length: 20, nullable: false })
+  @Column({ length: 20 })
+  @IsNotEmpty({ message: 'A senha é obrigatória' })
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
+  @MaxLength(20, { message: 'A senha deve ter no máximo 20 caracteres' })
   password: string;
 
-  @Column({ default: true, nullable: false })
+  @Column({ default: true })
   isActive: boolean;
 
   @Column({ enum: ['ADMIN', 'USER'], default: 'USER' })
